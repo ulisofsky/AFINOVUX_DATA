0e5b69a7c4a1f0c30c41a37279f282bce6e43af5
ulisofsky <ulisofsky@gmail.com>
2026-03-03 15:56:40 -0600

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 from openai import OpenAI
 
-from google.colab import drive.mount('/content/drive')
-
 # CONFIGURACIÓN DE PÁGINA
 st.set_page_config(
     page_title="El Santuario Solar",
